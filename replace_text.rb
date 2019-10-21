b318c59d1c4f0d77b83a28c56a07bdd39689459e
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-10-22 08:45:26 +0900

    🎉 initial commit
    指定したディレクトリ配下のfile名を全て取得する処理を作成

---
+++ replace_text.rb
@@ -0,0 +1,29 @@
+class FileGet
+  attr_accessor :filenames
+  def initialize
+    @filenames = []
+  end
+
+  def get_filenames(path)
+    Dir::foreach(path) do |v|
+      next if v == "." or v == ".."
+      if path =~ /\/$/
+        v = path + v
+      else
+        v = path + "/" + v
+      end
+
+      if FileTest::directory?(v)
+        get_filenames(v) #再帰呼び出し リカーシブ　コール/Recursive Call
+      else
+        # fileだった時のみ filenames に追加する
+        filenames <<  v 
+      end
+    end
+    filenames
+  end
+end
+
+fileget = FileGet.new
+filenames = fileget.get_filenames("/Users/yusuke/Rails/Glenfiddich/task_management/app/")
+p filenames
