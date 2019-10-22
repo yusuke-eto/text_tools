68c8b885f26e6d14cac4aadcbe676feacd0ee5d6
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-10-22 12:37:10 +0900

    📚 コメントを追加

--- replace_text_2.rb
+++ replace_text_2.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
+# path 配下の rubyファイルを絶対パスで全て取得する
+# 取得した path は配列に入って返ってくる
+# @return [Array]
 class FileGet
   def self.get_filenames(path)
+    # File.joinの説明
+    # 連結したいディレクトリ名やファイル名を文字列で与える
     rbfiles = File.join("**", "*.rb")
     Dir.glog(rbfiles, base: path)
   end
 end
 
+# ここで代入している path を適宜書き換えて使用する
 path = "hoge"
 puts FileGet.get_filenames(path)
